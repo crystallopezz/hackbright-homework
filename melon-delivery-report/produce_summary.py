f8d6ac9d3815c8b8adc75571df1bcd748908a66d
Crystal Lopez <crystallopez289@gmail.com>
2020-04-30 19:33:24 +0000

    fixed delivery report to say delivered COUNT MELONs for , turned repetitive code into a function

--- melon-delivery-report/produce_summary.py
+++ melon-delivery-report/produce_summary.py
@@ -1,43 +1,21 @@
-print("Day 1")
-the_file = open("um-deliveries-20140519.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
+def melon_delivery_report (day, file):
 
-    melon = words[0]
-    count = words[1]
-    amount = words[2]
+    print("Day", day)
 
-    print("Delivered {} {}s for total of ${}".format(
-        count, melon, amount))
-the_file.close()
+    the_file = open(file)
 
+    for line in the_file:
+        line = line.rstrip()
+        words = line.split('|')
 
-print("Day 2")
-the_file = open("um-deliveries-20140520.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
+        melon = words[0]
+        count = words[1]
+        amount = words[2]
 
-    melon = words[0]
-    count = words[0]
-    amount = words[0]
+        print("Delivered {} {}s for total of ${}".format(count, melon, amount))
 
-    print("Delivered {} {}s for total of ${}".format(
-        count, melon, amount))
-the_file.close()
+    the_file.close()
 
-
-print("Day 3")
-the_file = open("um-deliveries-20140521.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
-
-    melon = words[0]
-    count = words[0]
-    amount = words[0]
-
-    print("Delivered {} {}s for total of ${}".format(
-        count, melon, amount))
-the_file.close()
+melon_delivery_report(1, "um-deliveries-20140519.txt")
+melon_delivery_report(2, "um-deliveries-20140520.txt")
+melon_delivery_report(3, "um-deliveries-20140521.txt")
